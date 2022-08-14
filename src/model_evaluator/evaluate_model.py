5caa5d778519faefa2dbb02e643d2e56f82564b8
FraBoc123 <111138448+FraBoc123@users.noreply.github.com>
2022-08-14 18:44:39 -0400

    Update evaluate_model.py

--- src/model_evaluator/evaluate_model.py
+++ src/model_evaluator/evaluate_model.py
@@ -2,12 +2,20 @@ from sklearn.metrics import confusion_matrix, classification_report, precision_r
 
 
 class ModelEvaluator:
-
+    """
+    This class take the prediction and evaluates the model predictions
+    """
     def __init__(self):
         pass
 
     @classmethod
     def metrics_score(cls, actual, predicted):
+         """
+        Takes the actual data and predictions and return the evaluation
+        Param actual: Takes the data frame
+        Param predicted: the prediction as input
+        Return: Confusion Matrix, accuracy and F1 results
+        """
 
         cm = confusion_matrix(actual, predicted)
         accuracy = accuracy_score(actual, predicted)
