7cccf7b7a07830fd66ea10c573c5c5a1a88799d6
FraBoc123 <111138448+FraBoc123@users.noreply.github.com>
2022-08-14 18:49:41 -0400

    Update ml_svc.py

--- src/models/ml_svc.py
+++ src/models/ml_svc.py
@@ -8,8 +8,11 @@ from sklearn.impute import SimpleImputer
 
 
 class MLSupportVectorClassifier:
-
+    """Class that implements SVC classifier"""
     def __init__(self):
+        """
+        The constructor will initialize with columns and the oe hot encoder.
+        """
         self.clf = SVC(random_state=10, kernel='rbf', C=10)
         self.is_model_trained = False
         self.categorical_cols = ['Day', 'Venue']
@@ -18,6 +21,11 @@ class MLSupportVectorClassifier:
         self.num_imputer = SimpleImputer(strategy='median')
 
     def _fit_preprocessor(self, train_df):
+        """
+        Transforms the data frame from categorical to number(one-hot-encoder)
+        Param train_df: Panda array is expected
+        Return: a pandas dataframe
+        """
         transformed = self.one_hot_encoder.fit_transform(train_df[self.categorical_cols])
         train_df[np.concatenate(self.one_hot_encoder.categories_).ravel()] = transformed.toarray()
         train_df = train_df.drop(columns=self.categorical_cols)
@@ -27,6 +35,11 @@ class MLSupportVectorClassifier:
         return train_df
 
     def _transform_preprocessor(self, test_df):
+        """
+        Transforms testing Data into a one-hot-encoder on the testing
+        Param test_df: Part of the Data Frame for the training
+        Return: Result of the transformation of the data frame
+        """
         transformed = self.one_hot_encoder.transform(test_df[self.categorical_cols])
         test_df[np.concatenate(self.one_hot_encoder.categories_).ravel()] = transformed.toarray()
         test_df = test_df.drop(columns=self.categorical_cols)
@@ -36,12 +49,25 @@ class MLSupportVectorClassifier:
         return test_df
 
     def train(self, X_train, y_train):
+        """
+        Trains model
+        Param X_train: Part of the dataset that includes the features and every
+        single row is input into the model
+        Param y_train:Part of the data set that we compare
+        An iterable containing the observed outputs (each 0 or 1)
+        Return: A trained model
+        """
         X_train = self._fit_preprocessor(X_train)
         self.clf.fit(X_train, y_train)
         self.is_model_trained = True
         return self.is_model_trained
 
     def predict(self, X_test):
+        """
+        Runs model on test data
+        Param X_test: Partial data that was not used on the training
+        Return: Test the accuracy of the training
+        """
         X_test = self._transform_preprocessor(X_test)
         if not self.is_model_trained:
             print("Please train model before trying to predict")
